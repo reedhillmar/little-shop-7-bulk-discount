19465e7a5543d0d25c1a855aa66023543ceedef2
Captainlearyo <clayoleary@gmail.com>
2023-09-14 12:28:55 -0500

    create test helper

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -2,15 +2,15 @@ require "rails_helper"
 
 RSpec.describe "Merchant Dashboard show page", type: :feature do
   before :each do
-    @merchant_1 = Merchant.create!(name: "Ray's Handmade Jewelry")
+    load_test_data
   end
-
+  # Story 1
   it "displays the name of the merchant" do
     visit "/merchants/#{@merchant_1.id}/dashboard"
 
     expect(page).to have_content(@merchant_1.name)
   end
-
+  # Story 2
   it "displays a link to merchant items index" do
     visit "/merchants/#{@merchant_1.id}/dashboard"
 
@@ -26,4 +26,10 @@ RSpec.describe "Merchant Dashboard show page", type: :feature do
     click_link ("#{@merchant_1.name} Invoices")
     expect(current_path).to eq("/merchants/#{@merchant_1.id}/invoices")
   end
+
+  # Story 3
+  xit "shows top 5 customers with successful transactions" do
+  
+  end
+
 end
