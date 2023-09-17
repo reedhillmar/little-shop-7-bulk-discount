d8b754abca33abeb3a88d947ed8cc1138f49eb7a
Reed Hillmar <reedhillmar@gmail.com>
2023-09-17 14:18:08 -0600

    test: adds test code for US34

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -16,6 +16,19 @@ RSpec.describe "As a visitor when I visit 'admin/invoices/:id'" do
       expect(page).to have_content("Date Created: #{@invoice_1_c1.date_created}")
       expect(page).to have_content("Customer Name: #{@customer_1.full_name}")
     end
+  end
 
+  # user story 34
+  it "I see all items on the invoice with details" do
+    visit "admin/invoices/#{@invoice_1_c1.id}"
+
+    within("#invoice_items-information") do
+      within("#invoice_item-#{@invoice_item_1_i1_c1.id}") do
+        expect(page).to have_content(@invoice_item_1_i1_c1.item_name)
+        expect(page).to have_content("Quantity: #{@invoice_item_1_i1_c1.quantity}")
+        expect(page).to have_content("Unit Purchase Price: #{@invoice_item_1_i1_c1.unit_price}")
+        expect(page).to have_content("Status: #{@invoice_item_1_i1_c1.status}")
+      end
     end
   end
+end
