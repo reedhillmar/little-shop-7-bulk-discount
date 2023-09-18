0c1f7b2432049b0776faf0bc1b66d3521671287a
Charles Ren <cren04@syr.edu>
2023-09-17 22:36:49 -0700

    feat & test: US 16; visit merchant invoice show page to display all items on the invoice and the item attributes.

--- app/controllers/merchants/invoices_controller.rb
+++ app/controllers/merchants/invoices_controller.rb
@@ -2,10 +2,11 @@ class Merchants::InvoicesController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
     @invoices = @merchant.invoices
-    #@invoice_items = @invoices.invoice_items
   end
 
   def show
-
+    @merchant = Merchant.find(params[:merchant_id])
+    @invoice = Invoice.find(params[:invoice_id])
+    @invoice_items = InvoiceItem.joins(:item).where(invoice_id: params[:invoice_id], items: { merchant_id: @merchant.id })
   end
 end
---
+++ spec/features/merchants/invoices/show_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe "Merchant Invoice Show Page", type: :feature do
+  before :each do
+    load_test_data
+  end
+
+  # User Story 16
+  it "displays all items and their attributes(name, quantity, price, invoice item status) for that invoice" do
+    visit "/merchants/#{@merchant_5.id}/invoices/#{@invoice_3_c6.id}"
+
+    expect(page).to_not have_content(@item_1_m3.name)
+    expect(page).to have_content(@item_6_m5.name)
+    expect(page).to have_content( @invoice_item_1_i3_c6.quantity)
+    expect(page).to have_content(@item_6_m5.unit_price)
+    expect(page).to have_content(@invoice_item_1_i3_c6.status)
+  end 
+end
