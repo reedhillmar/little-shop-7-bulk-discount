fdadf098213808ea077439b42e462570f6b27a65
Reed Hillmar <reedhillmar@gmail.com>
2023-09-26 11:41:48 -0600

    fixing schema

--- db/migrate/20230924160032_add_discount_event_name_to_invoice_items.rb
+++
@@ -1,5 +0,0 @@
-class AddDiscountEventNameToInvoiceItems < ActiveRecord::Migration[7.0]
-  def change
-    add_column :invoice_items, :discount_event_name, :string, :default => "No Discount Applied"
-  end
-end
--- db/migrate/20230926173413_remove_discount_and_discount_event_name_from_invoice_items.rb
+++
@@ -1,6 +0,0 @@
-class RemoveDiscountAndDiscountEventNameFromInvoiceItems < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :invoice_items, :discount, :integer
-    remove_column :invoice_items, :discount_event_name, :string
-  end
-end
